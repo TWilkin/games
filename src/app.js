634e9c1dd39a08e38b011cfc68c850509ab980c0
TWilkin <tom@xzi-xzone.com>
2020-02-16 14:39:42 +0000

    Switched configuration to JSON file

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
+import config from 'config';
 import express from 'express';
-
-// the root path the application should listen at
-const ROOT_PATH = '/' + (process.env.ROOT_PATH  || '');
-
-// the port number to use or undefined for default
-const PORT = (process.env.PORT) ? parseInt(process.env.PORT) : undefined;
 
 // initialise express with a test route
 const app = express();
-app.get(ROOT_PATH, (_, res) => {
-    res.end('Hello World!');
+app.get(config.get('express.root'), (_, res) => {
+    res.end(config.get('message'));
 });
 
 // start listening
-let server = app.listen(PORT, () => {
-    console.log('Listening on http://%s:%s%s', server.address().address, server.address().port, ROOT_PATH);
+let server = app.listen(config.get('express.port'), () => {
+    console.log('Listening on http://%s:%s%s', server.address().address, server.address().port, config.get('express.root'));
 });
