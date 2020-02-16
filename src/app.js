16a8308f2979ff451f3efb3930431fba03b4def9
TWilkin <tom@xzi-xzone.com>
2020-02-16 14:30:40 +0000

    Added port override for local testing

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import express from 'express';
 // the root path the application should listen at
 const ROOT_PATH = '/' + (process.env.ROOT_PATH  || '');
 
+// the port number to use or undefined for default
+const PORT = (process.env.PORT) ? parseInt(process.env.PORT) : undefined;
+
+// initialise express with a test route
 const app = express();
-app.get(ROOT_PATH, (req, res) => {
+app.get(ROOT_PATH, (_, res) => {
     res.end('Hello World!');
 });
 
-// start listening to default host and port
-let server = app.listen(() => {
+// start listening
+let server = app.listen(PORT, () => {
     console.log('Listening on http://%s:%s%s', server.address().address, server.address().port, ROOT_PATH);
 });
