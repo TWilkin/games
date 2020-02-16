ad174711a89c3bddbf38e0d5ab66b259c1ce7104
TWilkin <tom@xzi-xzone.com>
2020-02-16 14:11:38 +0000

    Added root path environment variable

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 
+// the root path the application should listen at
+const ROOT_PATH = '/' + (process.env.ROOT_PATH  || '');
+
 const app = express();
-app.get('/test/', (req, res) => {
+app.get(ROOT_PATH, (req, res) => {
     res.end('Hello World!');
 });
 
 // start listening to default host and port
 let server = app.listen(() => {
-    console.log('Listening on http://%s,%s', server.address().address, server.address().port);
+    console.log('Listening on http://%s:%s%s', server.address().address, server.address().port, ROOT_PATH);
 });
