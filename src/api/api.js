4a36b6c6dcc3a16e48cfa5021ac6a2d14f456cfe
TWilkin <tom@twilkin.uk>
2020-02-20 21:03:09 +0000

    Added GET by id and PUT methods to API

--- src/api/api.js
+++ src/api/api.js
@@ -11,17 +11,34 @@ export function initAPI(app, root) {
 
     // create common endpoints for each model
     Object.values(sequelize.models).forEach((model) => {
-        const url = `${baseURL}/${model.name.toLowerCase()}/`;
+        const url = `${baseURL}/${model.name.toLowerCase()}`;
 
         // create the GET endpoint
         app.get(url, async (_, res) => {
             let result = await model.findAll();
             return res.json(result);
-        })
+        });
+
+        // create the GET by id endpoint
+        app.get(`${url}/:id`, async (req, res) => {
+            let result = await model.findByPk(req.params.id);
+            return res.json(result);
+        });
 
         // create the POST endpoint
         app.post(url, async (req, res) => {
             let result = await model.create(req.body);
+            return res.json(result);
+        });
+
+        // create the PUT endpoint
+        app.put(`${url}/:id`, async (req, res) => {
+            // create the query
+            const primaryKey = Object.values(model.rawAttributes).find(field => field.primaryKey).fieldName;
+            const query = { where: { }};
+            query.where[primaryKey] = req.params.id;
+
+            let result = await model.update(req.body, query);
             return res.json(result);
         });
     });
