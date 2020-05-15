ed8fe87671bafbd81e4d1e1134dd53ca1aae53ae
TWilkin <tom@xzi-xzone.com>
2020-05-15 23:19:08 +0100

    Added redirect to login page

--- ui/src/components/App/App.tsx
+++ ui/src/components/App/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import HttpStatus, { getStatusText } from 'http-status-codes';
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
 
 import Login from '../Login/Login';
 
-export default class Layout extends React.Component {
+type AppProps = {};
+
+interface AppState {
+    unauthorised?: boolean;
+}
+
+export default class App extends Component<AppProps, AppState> {
+
+    constructor(props: AppProps) {
+        super(props);
+
+        this.state = {
+            unauthorised: undefined
+        };
+    }
+
+    public static getDerivedStateFromError(error: Error) {
+        console.error(error);
+
+        // check if the error is 401/403
+        switch(error.message) {
+            case getStatusText(HttpStatus.FORBIDDEN):
+            case getStatusText(HttpStatus.UNAUTHORIZED):
+                return { unauthorised: true };
+        }
+    }
 
     public render() {
         return (
             <BrowserRouter>
+                {this.redirect()}
+
                 <div>
                     <nav>
                         <ul>
@@ -24,6 +52,15 @@ export default class Layout extends React.Component {
                 </div>
             </BrowserRouter>
         );
+    }
+
+    private redirect() {
+        if(this.state.unauthorised) {
+            this.setState({
+                unauthorised: undefined
+            });
+            return <Redirect to='/login' />;
+        }
     }
 
 }
