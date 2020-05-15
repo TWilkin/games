1fc30f0629cf5714824caaf89c027f29efbd6283
TWilkin <tom@xzi-xzone.com>
2020-05-15 22:29:26 +0100

    Added routes to UI

--- ui/src/components/App/App.tsx
+++ ui/src/components/App/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 import Login from '../Login/Login';
 
 export default class Layout extends React.Component {
 
     public render() {
-        return <Login apiUrl='http://localhost:3000/api' />;
+        return (
+            <BrowserRouter>
+                <div>
+                    <nav>
+                        <ul>
+                            <li><Link to='/login'>Login</Link></li>
+                        </ul>
+                    </nav>
+                    <hr />
+                
+                    <Switch>
+                        <Route path='/login'>
+                            <Login apiUrl='http://localhost:3000/api' />
+                        </Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
     }
 
 }
