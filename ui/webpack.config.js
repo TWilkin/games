0e9aa5d01e9b181d9464688a065b3a81fcc29341
TWilkin <tom@xzi-xzone.com>
2020-05-26 10:36:24 +0100

    Added ModalDialog component

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -21,6 +21,13 @@ module.exports = {
                 enforce: 'pre',
                 test: /\.js$/,
                 loader: 'source-map-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 
+                    'css-loader'
+                ]
             }
         ]
     },
