a8ba243a07ed2f8891f50daea637b8b6c2263203
TWilkin <4322355+TWilkin@users.noreply.github.com>
2020-12-30 12:36:13 +0000

    Fixed dev UI refresh not loading page with path

--- ui/webpack.common.js
+++ ui/webpack.common.js
@@ -32,6 +32,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     }
 };
