7380db7cb60bf2cd983112e3020920cf4a39a73b
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-01-01 22:44:42 +0000

    Added cover art to UI

---
+++ ui/src/components/Game/GameImage.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Game } from '../../models';
+
+import { APIProps } from '../common';
+
+interface GameImageProps extends APIProps {
+    game: Game;
+}
+
+export default class GameImage extends Component<GameImageProps> {
+    render() {
+        return (
+            <img src={`${this.props.api.url}/images/${this.props.game.gameId}}`}
+                alt={`Cover art for ${this.props.game.title}`} />
+        )
+    }
+};
