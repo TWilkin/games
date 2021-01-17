1e913874337d18b2aa002cd30a8a458663abfa60
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-01-17 22:57:55 +0000

    Add query by platformId to wishlist

--- api/src/models/game_wishlist.model.ts
+++ api/src/models/game_wishlist.model.ts
@@ -1,6 +1,6 @@
 import { AutoIncrement, BelongsTo, Column, DataType, ForeignKey, PrimaryKey, Table } from 'sequelize-typescript';
 
-import { Queryable } from '../api/decorators';
+import { NestedQueryable, Queryable } from '../api/decorators';
 import GamePlatform from './game_platform.model';
 import { AbstractOwnableModel } from './ownablemodel';
 
@@ -14,6 +14,7 @@ export default class GameWishlist extends AbstractOwnableModel<GameWishlist> {
     gameWishlistId!: number;
 
     @Queryable
+    @NestedQueryable('platformId')
     @ForeignKey(() => GamePlatform)
     @Column(DataType.INTEGER)
     gamePlatformId!: number;
