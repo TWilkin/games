6dfe1de59fb1c399a19ae8bc50e01b3298a6e61a
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-01-23 14:36:27 +0000

    Switch GameDetails to React hooks and add useMutation hook

--- ui/src/hooks/graphql.ts
+++ ui/src/hooks/graphql.ts
@@ -1,14 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { APISettings } from '../components/common';
 
-import query, { Query } from '../graphql';
+import query, { mutate, Query } from '../graphql';
 import { Model } from '../models';
 
 export function useQuery<TModel extends Model>(
     api: APISettings,
     graphqlQuery: Query, 
-    variables={},
-    props: any[]=[]): TModel[]
+    variables={}): TModel[]
 {
     const [ results, setResults ] = useState<TModel[]>(undefined);
 
@@ -20,7 +19,35 @@ export function useQuery<TModel extends Model>(
                 api.onError(error);
             }
         })();
-    }, props);
+    }, Object.keys(variables));
 
     return results;
+}
+
+export function useMutation<TModel extends Model>(
+    api: APISettings,
+    graphqlQuery: Query, 
+    variables={}): () => Promise<TModel>
+{
+    const [ isSending, setIsSending ] = useState(false);
+    const isMounted = useRef(true);
+
+    const sendRequest = useCallback(async () => {
+        if(isSending) {
+            return;
+        }
+
+        try {
+            setIsSending(true);
+            return await mutate<TModel>(api.url, graphqlQuery, variables);
+        } catch(error) {
+            api.onError(error);
+        } finally {
+            if(isMounted.current) {
+                setIsSending(false);
+            }
+        }
+    }, [isSending, ...Object.keys(variables)]);
+
+    return sendRequest;
 }
